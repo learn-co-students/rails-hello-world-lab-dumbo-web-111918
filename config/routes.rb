8405fb88942a7b3293acfe80b103168d21780755
John Mark Redding <johomurk@gmail.com>
2018-12-14 17:12:19 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
@@ -53,4 +52,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'index#index'
+
+  get '/hello_world' => 'index#hello_world'
+
 end
