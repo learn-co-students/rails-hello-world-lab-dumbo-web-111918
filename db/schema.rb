cc0e97228843f652dd732385954e26569e9cb72a
github username <github email address>
2018-12-17 01:33:14 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181217035320) do
+
+  create_table "statics", force: :cascade do |t|
+    t.string   "str1"
+    t.string   "str2"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
---
+++ spec/factories/statics.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :static do
+    str1 "MyString"
+    str2 "MyString"
+  end
+end
